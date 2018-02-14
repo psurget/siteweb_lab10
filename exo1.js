e2cc1136d70573e292e809814dcc72ee39f7ccac
psurget <surget.pascal@gmail.com>
2018-02-14 11:30:55 -0500

    Fin modif et test ex1

---
+++ exo1.js
@@ -0,0 +1,18 @@
+function initialiserCarte(){
+    if(!navigator.geolocation){
+        return false;
+    }
+    var centreGoogleMap = new google.maps.LatLng(45.483294, -73.868898);
+    var optionsGoogleMap = {
+        //facteur de zoom
+        zoom: 15,
+        center: centreGoogleMap,
+        /*Mode d'affichage de la carte
+        google.maps.MapTypeId.ROADTYPE
+        google.maps.MapTypeId.TERRAIN
+        google.maps.MapTypeId.SATELLITE
+        google.maps.MapTypeId.HYBRID*/
+        mapTypeId: google.maps.MapTypeId.ROADTYPE
+    };
+    var googleMap = new google.maps.Map(document.getElementById("googleMap"),optionsGoogleMap);
+}
